0fe7fa1b252ce5b556c4db0045cf80cd4c72c965
Vidar Magnusson <git@vidarmagnusson.com>
2022-11-26 01:22:25 +0100

    Create template project

---
+++ dayxx/src/solution.rs
@@ -0,0 +1,7 @@
+pub fn solve_part_one(input: &str) -> String {
+    todo!("Part one is not yet implemented");
+}
+
+pub fn solve_part_two(input: &str) -> String {
+    todo!("Part one is not yet implemented");
+}
