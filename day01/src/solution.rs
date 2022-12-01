f02e5136d47d5499ab83b348ead82488a87c9246
Vidar Magnusson <git@vidarmagnusson.com>
2022-12-01 06:18:27 +0100

    Solve day 1 2022

---
+++ day01/src/solution.rs
@@ -0,0 +1,70 @@
+pub fn solve_part_one(input: &str) -> String {
+    let lines = input
+        .lines()
+        .into_iter()
+        .map(|l| {
+            if l.is_empty() {
+                None
+            } else {
+                Some(u32::from_str_radix(l, 10).unwrap())
+            }
+        })
+        .collect::<Vec<Option<u32>>>();
+
+    let mut curr = 0;
+    let mut highest_elf = 0;
+    for calories in lines.into_iter() {
+        match calories {
+            None => {
+                if curr > highest_elf {
+                    highest_elf = curr;
+                }
+                curr = 0;
+            }
+            Some(n) => {
+                curr += n;
+            }
+        }
+    }
+    if curr > highest_elf {
+        highest_elf = curr;
+    }
+
+    format!("{}", highest_elf)
+}
+
+pub fn solve_part_two(input: &str) -> String {
+    let lines = input
+        .lines()
+        .into_iter()
+        .map(|l| {
+            if l.is_empty() {
+                None
+            } else {
+                Some(u32::from_str_radix(l, 10).unwrap())
+            }
+        })
+        .collect::<Vec<Option<u32>>>();
+
+    let mut elf_calories = Vec::new();
+    let mut curr = 0;
+    for calories in lines.into_iter() {
+        match calories {
+            None => {
+                elf_calories.push(curr);
+                curr = 0;
+            }
+            Some(n) => {
+                curr += n;
+            }
+        }
+    }
+    elf_calories.push(curr);
+
+    elf_calories.sort();
+    elf_calories.reverse();
+
+    let total = elf_calories[0] + elf_calories[1] + elf_calories[2];
+
+    format!("{}", total)
+}
