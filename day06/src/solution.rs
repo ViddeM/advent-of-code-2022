6e7ae9c9cdb368bbabcbad75581fe163d2d9ff20
Vidar Magnusson <git@vidarmagnusson.com>
2022-12-06 06:24:16 +0100

    Add day06

---
+++ day06/src/solution.rs
@@ -0,0 +1,25 @@
+pub fn parse<'a>(input: &'a str) -> &[u8] {
+    input.as_bytes()
+}
+
+fn find_packets(input: &[u8], window_size: usize) -> u32 {
+    'outer: for (index, arr) in input.windows(window_size).enumerate() {
+        for (i1, a) in arr.iter().enumerate() {
+            for (i2, b) in arr.iter().enumerate() {
+                if i1 != i2 && a == b {
+                    continue 'outer;
+                }
+            }
+        }
+        return (index + window_size) as u32;
+    }
+    panic!("Failed to find answer");
+}
+
+pub fn solve_part_one<'a>(input: &[u8]) -> String {
+    format!("{}", find_packets(input, 4))
+}
+
+pub fn solve_part_two<'a>(input: &[u8]) -> String {
+    format!("{}", find_packets(input, 14))
+}
